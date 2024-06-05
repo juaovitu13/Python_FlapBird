ce90fdd82049fd8f7363fded946afc830b9a2172
juaovitu13 <joao.victorsoares@outlook.com>
2024-06-05 16:49:28 -0300

    Função desenhar imagem do passaro feita

--- main.py
+++ main.py
@@ -60,6 +60,35 @@ class Passaro:
             if self.angulo > -90:
                 self.angulo -= self.VELOCIDADE_ROTACAO
 
+    def desenhar(self, tela):
+        # definir qual imagem do passaro vai usar
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
+            self.imagem = self.IMGS[0]
+            self.contagem_imagem = 0
+        
+        # se o passaro tiver caindo eu não vou bater asa
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_ANIMACAO*2
+
+        # desenhar a imagem
+        imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+        pos_centro_imagem = self.imagem.get_rect(topleft=(self.x, self.y)).center
+        retangulo = imagem_rotacionada.get_rect(center=pos_centro_imagem)
+        tela.blit(imagem_rotacionada, retangulo.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.imagem)
 
 class Cano:
     DISTANCIA = 200
